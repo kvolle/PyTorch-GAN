d70ea00f33a8c48da9a34db6c61b7405ff7f68f1
Keshav Raviprakash <keshra123@gmail.com>
2023-07-06 10:53:31 -0500

    Added Grid Image Method and Plotted the Losses

--- implementations/aae/aaenew.py
+++ implementations/aae/aaenew.py
@@ -23,13 +23,13 @@ import torch.optim as optim
 os.makedirs("images", exist_ok=True)
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--n_epochs", type=int, default=200, help="number of epochs of training")
+parser.add_argument("--n_epochs", type=int, default=100, help="number of epochs of training")
 parser.add_argument("--batch_size", type=int, default=8, help="size of the batches")
-parser.add_argument("--lr", type=float, default=0.0001, help="adam: learning rate")
+parser.add_argument("--lr", type=float, default=0.00005, help="adam: learning rate")
 parser.add_argument("--b1", type=float, default=0.5, help="adam: decay of first order momentum of gradient")
 parser.add_argument("--b2", type=float, default=0.999, help="adam: decay of first order momentum of gradient")
 parser.add_argument("--n_cpu", type=int, default=4, help="number of cpu threads to use during batch generation")
-parser.add_argument("--latent_dim", type=int, default=10, help="dimensionality of the latent code")
+parser.add_argument("--latent_dim", type=int, default=32, help="dimensionality of the latent code")
 parser.add_argument("--img_size", type=int, default=64, help="size of each image dimension")
 parser.add_argument("--channels", type=int, default=3, help="number of image channels")
 parser.add_argument("--sample_interval", type=int, default=400, help="interval between image sampling")
@@ -139,13 +139,6 @@ if cuda:
     adversarial_loss.cuda()
     pixelwise_loss.cuda()
 
-# Configure data loader
-#for param_tensor in encoder.state_dict():
-#    print(param_tensor, "\t", encoder.state_dict()[param_tensor].size())
-#for var_name in encoder.state_dict():
-#    print(var_name, "\t", encoder.state_dict()[var_name])
-#torch.save(encoder, 'encoder.pth')
-
 # Optimizers
 optimizer_G = torch.optim.Adam(
     itertools.chain(encoder.parameters(), decoder.parameters()), lr=opt.lr, betas=(opt.b1, opt.b2)
@@ -156,17 +149,84 @@ Tensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
 
 
 def sample_image(n_row, batches_done):
+    #load images from test dataset 
     """Saves a grid of generated digits"""
     # Sample noise
     z = Variable(Tensor(np.random.normal(0, 1, (n_row ** 2, opt.latent_dim))))
     gen_imgs = decoder(z)
+    #test = test_dataloader
+    real_imgs = Variable(imgs.type(Tensor))
+    #print(real_imgs.type)
+    latent = encoder(real_imgs)
+    #dec = decoder(latent) 
+    #latent_matrix = np.concatenate((latent_matrix,dec.cpu().numpy().squeeze()))
+    #latent_matrix = np.concatenate((latent_matrix,latent.cpu().numpy().squeeze()))
+    decoded = decoder(latent)
+    latent = Variable(decoded.type(Tensor))
     save_image(gen_imgs.data, "images/%d.png" % batches_done, nrow=n_row, normalize=True)
+    #latent captures dyanmics of the generatvie process: periodicity
+
+def grid_image(n_row, batches_done):
+    #load images from test dataset 
+    # Sample noise
+
+    #tensor by 24,3,64,64 (Two of them) => start with 0 by 3 by 64 by 64
+    decoded_collection = torch.empty([0,3,64,64]).cuda()
+    real_collection = torch.empty([0,3,64,64]).cuda()
+    #print(decoded_collection.shape)
+    print(real_collection)
+    with torch.no_grad():
+        for i, (imgs, _) in enumerate(test_dataloader):
+            #encoder.to('cuda')
+            if imgs.shape[0] == 8:
+                real_imgs = Variable(imgs.type(Tensor))
+                #print(real_imgs.shape)
+                real_collection = torch.cat((real_collection,real_imgs),0) 
+                #print("real collection loop: ",real_collection.shape)
+                #decode = torch.cat((real_collection,real_imgs),0)
+                #print("real collection loop: ",real_collection.shape)
+
+
+                
+                encoded_imgs = encoder(real_imgs)
+                #imgs_1 = Variable(Tensor(encoded_imgs)
+                decoded_imgs = Variable(Tensor(decoder(encoded_imgs)))
+                #print("Decoded Images: ", decoded_imgs.shape)
+                decoded_collection = torch.cat((decoded_collection,decoded_imgs),0)
+                #print("Decoded Collection: ",decoded_collection.shape)
+                #could be collection
+                #concatenate the two tensors; 
+                      
+
+
+
+         #imgs = Tensor(decoded_imgs)
+         #if decoded_imgs.shape[0] == 8:
+             
+         #print(decoded_imgs.shape)
+    #z = Variable(Tensor(np.random.normal(0, 1, (n_row ** 2, opt.latent_dim))))
+    #encoded_imgs = encoder(imgs)
+    #gen_imgs = decoder(z)
+    #torch.
+    #print(dist[i][mask[i] == 0])
+    #figure out shape of images matrix by 24 by 3 by 64 by 64
+    
+    
+    #print("real collection : ", real_collection.shape)
+    #print("decoded collection : ", decoded_collection.shape)
+    save_image(real_collection, "images/real.png", nrow=n_row, normalize=True)
+    save_image(decoded_collection, "images/decoded.png", nrow=n_row, normalize=True)
+
 
 #test data concatenate latent space 
 # ----------
 #  Training
 # ----------
 if TRAIN:
+    plt.figure("Gen Loss")
+    gen = plt.gca()
+    plt.figure("Dis Loss")
+    dis = plt.gca()
     for epoch in range(opt.n_epochs):
         for i, (imgs, _) in enumerate(train_dataloader):
             # net = Net()
@@ -221,10 +281,14 @@ if TRAIN:
                 "[Epoch %d/%d] [Batch %d/%d] [D loss: %f] [G loss: %f]"
                 % (epoch, opt.n_epochs, i, len(train_dataloader), d_loss.item(), g_loss.item())
             )
-
+            if i == 59:
+                gen.plot(epoch, g_loss.item(),'bo')
+                dis.plot(epoch, d_loss.item(),'bo')
+            
             batches_done = epoch * len(train_dataloader) + i
             if batches_done % opt.sample_interval == 0:
                 sample_image(n_row=10, batches_done=batches_done)
+                grid_image(n_row=10, batches_done=batches_done)
 
     torch.save(encoder, 'encoder.pth')
     torch.save(decoder,'decoder.pth')
@@ -235,16 +299,42 @@ else:
     discriminator = torch.load('discriminator.pth')
 latent_matrix = np.empty((0,opt.latent_dim))
 with torch.no_grad():
-    for i, (imgs, _) in enumerate(train_dataloader):
+    for i, (imgs, _) in enumerate(test_dataloader):
         #test = test_dataloader
         real_imgs = Variable(imgs.type(Tensor))
+        print(real_imgs.type)
         latent = encoder(real_imgs)
         latent_matrix = np.concatenate((latent_matrix,latent.cpu().numpy().squeeze()))
+        decoded = decoder(latent)
+        latent = Variable(decoded.type(Tensor))
+        
+    #latent_matrix = np.empty((0,opt.latent_dim))
+#with torch.no_grad():
 fig_1, ax_1 = plt.subplots(3,3)
 fig_2, ax_2 = plt.subplots(3,3)
 ax_1 = ax_1.ravel()
 ax_2 = ax_2.ravel()
 for i in range(9):
+        #ax_1[i].hist(latent_matrix[i])
+        #ax_2[i].plot(latent_matrix[i])
     ax_1[i].hist(latent_matrix[:,i])
     ax_2[i].plot(latent_matrix[:,i])
 plt.show()
+#save them to grid and the 6 * 4 grid of images with real and reconstruced by decoder images
+# loop over test dataset => feed everything throguh the encoder and decoder
+#concatentate torhc tensors in torch 
+#if imgs.shape[0] == 8:
+
+"""
+def grid_image(n_row, batches_done):
+    #load images from test dataset 
+    # Sample noise
+    for i, (imgs, _) in enumerate(test_dataloader):
+         encoded_imgs = encoder(imgs)
+         decoded_imgs = decoder(encoded_imgs)
+         print(decoded_imgs.shape)
+    #z = Variable(Tensor(np.random.normal(0, 1, (n_row ** 2, opt.latent_dim))))
+    #encoded_imgs = encoder(imgs)
+    #gen_imgs = decoder(z)
+    save_image(decoded_imgs.data, "images/%d.png" % batches_done, nrow=n_row, normalize=True)
+    """
